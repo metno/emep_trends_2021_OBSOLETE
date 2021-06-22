f6a6f59f8d154136782b5e5fa2d639b3e19f00fa
jgliss <jonasg@met.no>
2021-06-22 13:40:46 +0200

    finish first draft for obs trends script and output

--- calc_obstrends.py
+++ calc_obstrends.py
@@ -19,27 +19,28 @@ DEFAULT_RESAMPLE_CONSTRAINTS = dict(monthly     =   dict(daily      = 21),
 
 DEFAULT_RESAMPLE_HOW = 'mean'
 
-PERIODS = [(2000,2020, 14),
-           (2000,2010, 7),
-           (2010,2020, 7),]
+PERIODS = [(2000, 2019, 14),
+           (2000, 2010, 7),
+           (2010, 2019, 7)]
 
 EBAS_VARS = ['concso2',
-             'conco3',
-             'concso4',
-             'concoc',
-            'concCec',
-            'conctc',
-            'concss',
-            'concnh3',
-            'concnh4',
-            'concNhno3',
-            'concNtno3',
-            'concNtnh',
-            'concno2',
-            'concpm10',
-            'concpm25',
-            'sc550dryaer',
-            'ac550aer'
+# =============================================================================
+#              'concso4',
+#              'concoc',
+#             'concCec',
+#             'conctc',
+#             'concss',
+#             'concnh3',
+#             'concnh4',
+#             'concNhno3',
+#             'concNtno3',
+#             'concNtnh',
+#             'concno2',
+#             'concpm10',
+#             'concpm25',
+#             'sc550dryaer',
+#             'ac550aer'
+# =============================================================================
             ]
 
 EBAS_BASE_FILTERS = dict(set_flags_nan   = True,
@@ -74,6 +75,7 @@ if __name__ == '__main__':
 
     for var in EBAS_VARS:
         sitemeta = []
+        trendtab = []
 
         data = oreader.read(vars_to_retrieve=var)
         data = data.apply_filters(**EBAS_BASE_FILTERS)
@@ -83,38 +85,51 @@ if __name__ == '__main__':
                                             resample_how=DEFAULT_RESAMPLE_HOW,
                                             min_num_obs=DEFAULT_RESAMPLE_CONSTRAINTS)
         tst = 'monthly'
-        print(var)
-        for site in tqdm.tqdm(sitedata['stats']):
+        for site in tqdm.tqdm(sitedata['stats'], desc=var):
+            try:
+                site = site.resample_time(
+                    var_name=var,
+                    ts_type=tst,
+                    min_num_obs=DEFAULT_RESAMPLE_CONSTRAINTS,
+                    how=DEFAULT_RESAMPLE_HOW)
+            except pya.exceptions.TemporalResolutionError:
+                continue # lower res than monthly
 
-            site = site.resample_time(var, tst,
-                                      min_num_obs=DEFAULT_RESAMPLE_CONSTRAINTS,
-                                      how=DEFAULT_RESAMPLE_HOW)
             ts = site[var].loc[start_yr:stop_yr]
             if len(ts) == 0 or np.isnan(ts).all(): # skip
                 continue
             subdir = os.path.join(OUTPUT_DIR, f'data_{var}')
+
+            site_id = site.station_id
             os.makedirs(subdir, exist_ok=True)
-            fname = f'data_{var}_{site.station_id}_{tst}.csv'
+            fname = f'data_{var}_{site_id}_{tst}.csv'
 
             siteout = os.path.join(subdir, fname)
             ts.to_csv(siteout)
+            unit = site.get_unit(var)
             sitemeta.append([var,
-                             site.station_id,
+                             site_id,
                              site.station_name,
                              site.latitude,
                              site.longitude,
                              site.altitude,
-                             site.get_unit(var),
+                             unit,
                              tst])
 
             te = pya.trends_engine.TrendsEngine
-            resulttab = []
 
 
             for (start, stop, min_yrs) in PERIODS:
                 for seas in SEASONS:
                     trend = te.compute_trend(ts, tst, start, stop, min_yrs,
                                              seas)
+
+                    row = [var, site_id, trend['period'], trend['season'],
+                           trend[f'slp_{start}'], trend[f'slp_{start}_err'],
+                           trend[f'reg0_{start}'], trend['m'], trend['m_err'],
+                           trend['n'], trend['pval'], unit]
+
+                    trendtab.append(row)
 
         metadf = pd.DataFrame(sitemeta,
                               columns=['var',
@@ -129,6 +144,25 @@ if __name__ == '__main__':
         metaout = os.path.join(OUTPUT_DIR, f'sitemeta_{var}.csv')
 
         metadf.to_csv(metaout)
+
+        trenddf = pd.DataFrame(trendtab,
+                               columns=['var',
+                                       'station_id',
+                                       'period',
+                                       'season',
+                                       'trend [%/yr]',
+                                       'trend err [%/yr]',
+                                       'yoffs',
+                                       'slope',
+                                       'slope err',
+                                       'num yrs',
+                                       'pval',
+                                       'unit'
+                                       ])
+
+        trendout = os.path.join(OUTPUT_DIR, f'trends_{var}.csv')
+
+        trenddf.to_csv(trendout)
 
 
 
