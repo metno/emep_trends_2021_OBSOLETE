f4ced9072accd009f2c034edceee3b41b0434dd1
jgliss <jonasg@met.no>
2021-06-22 16:26:00 +0200

    run a bunch of obs vars for testing

--- calc_obstrends_o3.py
+++ calc_obstrends_o3.py
@@ -9,40 +9,27 @@ import os, tqdm
 import numpy as np
 import pandas as pd
 import pyaerocom as pya
-from pyaerocom.trends_helpers import SEASONS
 
 EBAS_LOCAL = '/home/jonasg/MyPyaerocom/data/obsdata/EBASMultiColumn/data'
 EBAS_ID = 'EBASMC'
 
-DEFAULT_RESAMPLE_CONSTRAINTS = dict(monthly     =   dict(daily      = 21),
+DEFAULT_RESAMPLE_CONSTRAINTS = dict(yearly     =   dict(daily      = 270),
                                     daily       =   dict(hourly     = 18))
 
-DEFAULT_RESAMPLE_HOW = 'mean'
+RESAMPLE_HOW = dict(daily       =   dict(hourly='max'))
+
+PERECENTILES = [10, 50, 75, 95, 98, 99]
+def get_rs_how(percentile):
+    rs_how = {**RESAMPLE_HOW}
+    rs_how['yearly'] = dict(daily=f'{percentile}percentile')
+    return rs_how
+
 
 PERIODS = [(2000, 2019, 14),
            (2000, 2010, 7),
            (2010, 2019, 7)]
 
-EBAS_VARS = ['concso4',
-             'concno2',
-             'concpm10',
-             'concpm25',
-# =============================================================================
-#              'concoc',
-#             'concCec',
-#             'conctc',
-#             'concss',
-#             'concnh3',
-#             'concnh4',
-#             'concNhno3',
-#             'concNtno3',
-#             'concNtnh',
-#             ,
-#
-#             'sc550dryaer',
-#             'ac550aer'
-# =============================================================================
-            ]
+EBAS_VARS = ['conco3']
 
 EBAS_BASE_FILTERS = dict(set_flags_nan   = True,
                          data_level      = 2)
@@ -83,27 +70,29 @@ if __name__ == '__main__':
         #data = data.apply_filters(station_name='Birkenes II')
 
         sitedata = data.to_station_data_all(var,
-                                            resample_how=DEFAULT_RESAMPLE_HOW,
+                                            resample_how=RESAMPLE_HOW,
                                             min_num_obs=DEFAULT_RESAMPLE_CONSTRAINTS)
-        tst = 'monthly'
+
         for site in tqdm.tqdm(sitedata['stats'], desc=var):
+            tst = 'daily'
             try:
                 site = site.resample_time(
                     var_name=var,
                     ts_type=tst,
                     min_num_obs=DEFAULT_RESAMPLE_CONSTRAINTS,
-                    how=DEFAULT_RESAMPLE_HOW)
+                    how=RESAMPLE_HOW)
             except pya.exceptions.TemporalResolutionError:
                 continue # lower res than monthly
 
             ts = site[var].loc[start_yr:stop_yr]
             if len(ts) == 0 or np.isnan(ts).all(): # skip
                 continue
+
             subdir = os.path.join(OUTPUT_DIR, f'data_{var}')
 
             site_id = site.station_id
             os.makedirs(subdir, exist_ok=True)
-            fname = f'data_{var}_{site_id}_{tst}.csv'
+            fname = f'{var}_{site_id}_{tst}.csv'
 
             siteout = os.path.join(subdir, fname)
             ts.to_csv(siteout)
@@ -117,18 +106,34 @@ if __name__ == '__main__':
                              unit,
                              tst])
 
-            te = pya.trends_engine.TrendsEngine
+            tst = 'yearly'
+            for percentile in PERECENTILES:
+                rs_how = get_rs_how(percentile)
+                try:
+                    site = site.resample_time(
+                        var_name=var,
+                        ts_type=tst,
+                        min_num_obs=DEFAULT_RESAMPLE_CONSTRAINTS,
+                        how=rs_how)
+                except pya.exceptions.TemporalResolutionError:
+                    continue # lower res than monthly
+
+                ts = site[var]
+                if len(ts) == 0 or np.isnan(ts).all(): # skip
+                    continue
+
+                te = pya.trends_engine.TrendsEngine
 
 
-            for (start, stop, min_yrs) in PERIODS:
-                for seas in SEASONS:
+                for (start, stop, min_yrs) in PERIODS:
+
                     trend = te.compute_trend(ts, tst, start, stop, min_yrs,
-                                             seas)
+                                             'all')
 
                     row = [var, site_id, trend['period'], trend['season'],
                            trend[f'slp_{start}'], trend[f'slp_{start}_err'],
                            trend[f'reg0_{start}'], trend['m'], trend['m_err'],
-                           trend['n'], trend['pval'], unit]
+                           trend['n'], trend['pval'], unit, percentile]
 
                     trendtab.append(row)
 
@@ -158,7 +163,8 @@ if __name__ == '__main__':
                                        'slope err',
                                        'num yrs',
                                        'pval',
-                                       'unit'
+                                       'unit',
+                                       'percentile'
                                        ])
 
         trendout = os.path.join(OUTPUT_DIR, f'trends_{var}.csv')
